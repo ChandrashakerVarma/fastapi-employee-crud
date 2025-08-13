b8e3b6b62e687b58d3b1857ab7d12fbe9ee7022f
ChandrashakerVarma <csvarma073@gmail.com>
2025-08-13 11:12:13 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -7,13 +7,10 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-# create sync engine for SQLAlchemy (not async)
 engine = create_engine(DATABASE_URL, echo=True, pool_pre_ping=True)
 
-# session factory
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# declarative base
 Base = declarative_base()
 
 # dependency for FastAPI endpoints
